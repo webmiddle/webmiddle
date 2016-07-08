a5e928f2f01ea66b35211697a8b620eee983de60
Manuel Dell'Elce <mh025r0x@gmail.com>
2016-07-08 10:33:52 +0200

    Prevent infinite loop when passing NaN to evaluate

--- src/utils/evaluate.js
+++ src/utils/evaluate.js
@@ -1,21 +1,31 @@
 import isVirtual from './isVirtual';
 import isResource from './isResource';
 
+// check if target is the actual NaN value:
+// NaN is the only value that is not equal to itself.
+function isRealNaN(target) {
+  return target !== target; // eslint-disable-line no-self-compare
+}
+
 export default async function evaluate(value, options = {}) {
   try {
+    //console.log('evaluate', value);
     let result = value;
 
     if (typeof result === 'function') {
+      //console.log('evaluate function');
       result = result(...(options.functionParameters || []));
       return this.evaluate(result, options);
     }
 
     const promiseResult = await Promise.resolve(result);
-    if (promiseResult !== result) {
+    if (promiseResult !== result && (!isRealNaN(promiseResult) || !isRealNaN(result))) {
+      //console.log('evaluate promise result', promiseResult, result);
       return this.evaluate(promiseResult, options);
     }
 
     if (isVirtual(result)) {
+      //console.log('evaluate virtual');
       const { result: virtualResult, webmiddle } = await this.callVirtual(result);
       if (virtualResult !== result) {
         return webmiddle.evaluate(virtualResult, options);
