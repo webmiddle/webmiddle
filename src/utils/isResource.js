5ba07fd5b3481d249d2b07111594454aa8979336
Manuel Dell'Elce <mh025r0x@gmail.com>
2016-09-15 13:01:01 +0200

    Fix isResource, isVirtual

--- src/utils/isResource.js
+++ src/utils/isResource.js
@@ -1,6 +1,7 @@
 import isEqual from 'lodash.isequal';
+import sortBy from 'lodash.sortby';
 
 export default function isResource(target) {
   return typeof target === 'object' && target !== null &&
-    isEqual(Object.keys(target), ['name', 'contentType', 'content']);
+    isEqual(sortBy(Object.keys(target)), sortBy(['name', 'contentType', 'content']));
 }
--- src/utils/isVirtual.js
+++ src/utils/isVirtual.js
@@ -1,7 +1,8 @@
 import isEqual from 'lodash.isequal';
+import sortBy from 'lodash.sortby';
 
 // https://github.com/dekujs/virtual-element/blob/master/index.js#L30
 export default function isVirtual(target) {
   return typeof target === 'object' && target !== null &&
-    isEqual(Object.keys(target), ['type', 'attributes', 'children']);
+    isEqual(sortBy(Object.keys(target)), sortBy(['type', 'attributes', 'children']));
 }
