2f9e9e78575c7e4164eb9e4c8e7fce0b90407903
Manuel Dell'Elce <mh025r0x@gmail.com>
2016-09-22 12:00:45 +0200

    Remove catch, set propTypes

--- packages/webmiddle-service-parallel/src/Parallel.js
+++ packages/webmiddle-service-parallel/src/Parallel.js
@@ -1,31 +1,26 @@
 import WebMiddle, { PropTypes } from 'webmiddle';
 
-const Parallel = ({ name, children, webmiddle }) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const resources = {};
-      const promises = [];
+const Parallel = async ({ name, children, webmiddle }) => {
+  const resources = {};
+  const promises = [];
 
-      for (const child of children) {
-        const promise = webmiddle.evaluate(child, {
-          expectResource: true,
-        }).then(result => {
-          resources[result.name] = result;
-        });
-        promises.push(promise);
-      }
+  for (const child of children) {
+    const promise = webmiddle.evaluate(child, {
+      expectResource: true,
+    }).then(result => {
+      resources[result.name] = result;
+    });
+    promises.push(promise);
+  }
 
-      await Promise.all(promises);
-      resolve({ name, contentType: 'application/json', content: resources });
-    } catch (e) {
-      console.log('Parallel', e);
-      reject(e);
-    }
-  });
+  await Promise.all(promises);
+  return { name, contentType: 'application/json', content: resources };
 };
 
 Parallel.propTypes = {
   name: PropTypes.string.isRequired,
+  children: PropTypes.array.isRequired,
+  webmiddle: PropTypes.object.isRequired,
 };
 
 export default Parallel;
