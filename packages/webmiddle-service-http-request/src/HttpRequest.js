b51be7e2e2bf2e7a7d2f59ca66210b51f7c41150
Manuel Dell'Elce <mh025r0x@gmail.com>
2016-09-22 12:18:32 +0200

    Reject in case of unhandled exceptions

--- packages/webmiddle-service-http-request/src/HttpRequest.js
+++ packages/webmiddle-service-http-request/src/HttpRequest.js
@@ -5,44 +5,48 @@ const HttpRequest =
 ({ name, contentType, url, method = 'GET', body = {}, httpHeaders = {}, cookies = {} }) => {
   // TODO: cookies
   return new Promise((resolve, reject) => {
-    const isJsonBody = httpHeaders && httpHeaders['Content-Type'] === 'application/json';
+    try {
+      const isJsonBody = httpHeaders && httpHeaders['Content-Type'] === 'application/json';
 
-    if (typeof body === 'object' && body !== null) {
-      // body as string
-      if (isJsonBody) {
-        body = JSON.stringify(body);
-      } else {
-        // default: convert to form data
-        body = Object.keys(body).reduce((list, prop) => {
-          const value = body[prop];
-          list.push(`${encodeURIComponent(prop)}=${encodeURIComponent(value)}`);
-          return list;
-        }, []).join('&');
+      if (typeof body === 'object' && body !== null) {
+        // body as string
+        if (isJsonBody) {
+          body = JSON.stringify(body);
+        } else {
+          // default: convert to form data
+          body = Object.keys(body).reduce((list, prop) => {
+            const value = body[prop];
+            list.push(`${encodeURIComponent(prop)}=${encodeURIComponent(value)}`);
+            return list;
+          }, []).join('&');
+        }
       }
+
+      request({
+        uri: url,
+        method,
+        body,
+        headers: {
+          // default form content type needs to be explicitly set
+          // (request doesn't do it automatically when using the body property)
+          'Content-Type': !isJsonBody ? 'application/x-www-form-urlencoded' : undefined,
+          ...httpHeaders,
+        },
+        jar: true, // remember cookies for future use
+      }, (error, response, content) => {
+        if (!error && response.statusCode === 200) {
+          resolve({
+            name,
+            contentType,
+            content: (contentType === 'application/json') ? JSON.parse(content) : content,
+          });
+        } else {
+          reject(error || response.statusCode);
+        }
+      });
+    } catch (err) {
+      reject(err);
     }
-
-    request({
-      uri: url,
-      method,
-      body,
-      headers: {
-        // default form content type needs to be explicitly set
-        // (request doesn't do it automatically when using the body property)
-        'Content-Type': !isJsonBody ? 'application/x-www-form-urlencoded' : undefined,
-        ...httpHeaders,
-      },
-      jar: true, // remember cookies for future use
-    }, (error, response, content) => {
-      if (!error && response.statusCode === 200) {
-        resolve({
-          name,
-          contentType,
-          content: (contentType === 'application/json') ? JSON.parse(content) : content,
-        });
-      } else {
-        reject(error || response.statusCode);
-      }
-    });
   });
 };
 
