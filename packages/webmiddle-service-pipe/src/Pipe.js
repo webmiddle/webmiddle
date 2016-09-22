155decfed658ffdf53ca6f1eeeb9ff54f13326c8
Manuel Dell'Elce <mh025r0x@gmail.com>
2016-09-22 11:54:19 +0200

    Remove catch, set propTypes

--- packages/webmiddle-service-pipe/src/Pipe.js
+++ packages/webmiddle-service-pipe/src/Pipe.js
@@ -1,30 +1,26 @@
 import WebMiddle, { PropTypes } from 'webmiddle';
 
 const Pipe = ({ children, webmiddle }) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const resources = {};
-      let lastResource;
+  return Promise.resolve().then(async () => {
+    const resources = {};
+    let lastResource;
 
-      for (const child of children) {
-        const resource = await webmiddle.evaluate(child, {
-          expectResource: true,
-          functionParameters: [resources],
-        });
-        resources[resource.name] = resource;
-        lastResource = resource;
-      }
-
-      resolve(lastResource);
-    } catch (e) {
-      console.log('Pipe', e);
-      reject(e);
+    for (const child of children) {
+      const resource = await webmiddle.evaluate(child, {
+        expectResource: true,
+        functionParameters: [resources],
+      });
+      resources[resource.name] = resource;
+      lastResource = resource;
     }
+
+    return lastResource;
   });
 };
 
 Pipe.propTypes = {
-
+  children: PropTypes.array.isRequired,
+  webmiddle: PropTypes.object.isRequired,
 };
 
 export default Pipe;
