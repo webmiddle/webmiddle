4786affba18ba8ee798e7504af29e2722250af10
Manuel Dell'Elce <mh025r0x@gmail.com>
2017-04-15 20:37:15 +0200

    webmiddle-manager-cookie: tests

--- packages/webmiddle-manager-cookie/tests/index.js
+++ packages/webmiddle-manager-cookie/tests/index.js
@@ -1,9 +1,13 @@
 import test from 'ava';
+import CookieManager from '../src/index';
 
 test.beforeEach(t => {
 
 });
 
 test('main', async t => {
-  t.pass();
+  const cookieManager = new CookieManager();
+
+  t.truthy(cookieManager.jar);
+  t.truthy(cookieManager.Cookie);
 });
