13124445a9730c1e08555cd1487177df2f7f13fb
Manuel Dell'Elce <mh025r0x@gmail.com>
2017-04-19 13:12:28 +0200

    webmiddle-server: add tests

--- packages/webmiddle-server/tests/index.js
+++ packages/webmiddle-server/tests/index.js
@@ -1,33 +1,117 @@
 import test from 'ava';
 import Server from '../src';
 import WebMiddle from 'webmiddle';
+import superagent from 'superagent';
 
-test.beforeEach(t => {
+function requestServer(method, url, data = {}) {
+  return new Promise((resolve, reject) => {
+    superagent[method.toLowerCase()](url)
+      .send(data)
+      .end((err, res) => {
+        if (err) reject(err);
+        resolve(res.body);
+      });
+  });
+}
 
+const PORT = 3000;
+
+const webmiddle = new WebMiddle({
+  services: {
+    math: {
+      sum: ({ a, b }) => ({
+        name: 'result',
+        contentType: 'text/plain',
+        content: String(Number(a) + Number(b)), // without Number() a + b would be a string concatenation in GET requests!
+      }),
+
+      multiply: ({ a, b }) => ({
+        name: 'result',
+        contentType: 'text/plain',
+        content: String(Number(a) * Number(b)),
+      }),
+
+      divide: ({ a, b }) => ({
+        name: 'result',
+        contentType: 'text/plain',
+        content: String(Number(a) / Number(b)),
+      }),
+    },
+    returnOption: ({ optionName }, context) => context.options[optionName],
+  },
+  settings: {
+    foo: {
+      some: 'bar',
+      other: 100,
+    },
+  },
 });
 
-test('main', async t => {
-  const parentWebmiddle = new WebMiddle({
-    services: {
+const server = new Server(webmiddle, { port: PORT });
+server.start();
 
+test('Execute service via GET', async t => {
+  const resource = await requestServer('GET', 'http://localhost:' + PORT + '/services/math/sum?a=5&b=10');
+  t.deepEqual(resource, {
+    name: 'result',
+    contentType: 'text/plain',
+    content: '15',
+  });
+});
+
+test('Execute service via POST', async t => {
+  const resource = await requestServer('POST', 'http://localhost:' + PORT + '/services/math/multiply', {
+    props: {
+      a: 20,
+      b: 5,
+    }
+  });
+  t.deepEqual(resource, {
+    name: 'result',
+    contentType: 'text/plain',
+    content: '100',
+  });
+});
+
+test('Pass context options to service via POST', async t => {
+  const resource = await requestServer('POST', 'http://localhost:' + PORT + '/services/returnOption', {
+    props: {
+      optionName: 'custom'
     },
-    settings: {
-      some: 'other',
+    options: {
+      custom: 5
     },
   });
+  t.is(resource.contentType, 'x-webmiddle-any');
+  t.is(resource.content, 5);
+});
 
-  const webmiddle = new WebMiddle({
-    parent: parentWebmiddle,
-    services: {
+test('Read setting via GET', async t => {
+  const resource = await requestServer('GET', 'http://localhost:' + PORT + '/settings/foo/other');
+  t.is(resource.contentType, 'x-webmiddle-any');
+  t.is(resource.content, 100);
+});
 
-    },
-    settings: {
-      foo: 'bar',
-    },
+test('Read setting via POST', async t => {
+  const resource = await requestServer('GET', 'http://localhost:' + PORT + '/settings/foo/other');
+  t.is(resource.contentType, 'x-webmiddle-any');
+  t.is(resource.content, 100);
+});
+
+test('Get service paths', async t => {
+  const resource = await requestServer('GET', 'http://localhost:' + PORT + '/services/');
+  t.deepEqual(resource, {
+    name: 'services',
+    contentType: 'application/json',
+    content: ['math.sum', 'math.multiply', 'math.divide', 'returnOption']
   });
+});
 
-  const server = new Server(webmiddle);
-  server.start();
-
-  t.pass();
+test('Get setting paths', async t => {
+  const resource = await requestServer('GET', 'http://localhost:' + PORT + '/settings/');
+  t.deepEqual(resource, {
+    name: 'settings',
+    contentType: 'application/json',
+    content: ['foo.some', 'foo.other']
+  });
 });
