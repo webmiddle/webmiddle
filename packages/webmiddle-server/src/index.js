3e1a3a823e3ce1b92a54bee550ce8a8ee80291a1
Manuel Dell'Elce <mh025r0x@gmail.com>
2017-05-07 13:48:30 +0200

    webmiddle-server: websocket implementation

--- packages/webmiddle-server/src/index.js
+++ packages/webmiddle-server/src/index.js
@@ -3,6 +3,13 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import _ from 'lodash';
+import WebSocket from 'ws';
+import uuid from 'uuid';
+
+function httpToServicePath(path) {
+  if (path.startsWith('/')) path = path.slice(1);
+  return path.replace(/\//g, '.');
+}
 
 function paths(obj, parentKey) {
   if (typeof obj !== 'object' || obj === null) return [parentKey];
@@ -19,10 +26,11 @@ export default class Server {
 
     this.PORT = options.port || 3000;
     this.expressServer = express();
+    this.websocketServer = null;
 
     this.handlersByType = {
-      services: this.handleService,
-      settings: this.handleSetting,
+      services: this._handleService,
+      settings: this._handleSetting,
     };
   }
 
@@ -31,54 +39,114 @@ export default class Server {
     this.expressServer.use(bodyParser.json({ limit: '50mb' }));
     this.expressServer.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 
-    this.expressServer.listen(this.PORT, () => {
+    const httpServer = this.expressServer.listen(this.PORT, () => {
       console.log(`WebMiddle server listening on port ${this.PORT}!`);
     });
 
-    ['services', 'settings'].forEach(type => this._bind(type));
+    this.websocketServer = new WebSocket.Server({ server: httpServer });
+
+    this._bindExpress();
+    this._bindWebsocket();
   }
 
-  // param type can be either "services" or "settings"
-  _bind(type) {
-    ['GET', 'POST'].forEach(httpMethod => {
-      const expressMethod = this.expressServer[httpMethod.toLowerCase()];
-      expressMethod.call(this.expressServer, `/${type}/*`, async (req, res) => {
-        try {
-          let path = req.url.split('?')[0].slice(('/' + type).length);
-          if (path.startsWith('/')) path = path.slice(1);
-          path = path.replace(/\//g, '.');
+  _bindExpress() {
+    Object.keys(this.handlersByType).forEach(type => {
+      ['GET', 'POST'].forEach(httpMethod => {
+        const expressMethod = this.expressServer[httpMethod.toLowerCase()];
+        expressMethod.call(this.expressServer, `/${type}/*`, async (req, res) => {
+          try {
+            const httpPath = req.url.split('?')[0].slice(('/' + type).length);
+            const path = httpToServicePath(httpPath);
 
-          let props;
-          let options;
-          if (httpMethod === 'GET') {
-            props = req.query || {};
-            options = {};
-          } else {
-            props = req.body.props || {};
-            options = req.body.options || {};
-          }
+            let props;
+            let options;
+            if (httpMethod === 'GET') {
+              props = req.query || {};
+              options = {};
+            } else {
+              props = req.body.props || {};
+              options = req.body.options || {};
+            }
 
-          const output = await this.handlersByType[type].call(this, path, props, options);
-          if (isResource(output)) {
-            res.json(output);
-          } else {
-            // wrap the output into a resource anyway, since we always want to send json
-            // (to handle things like numeric content and to simplify the client job)
-            res.json({
-              name: 'wrappedContent',
-              contentType: 'x-webmiddle-any',
-              content: output,
-            });
+            const output = await this.handlersByType[type].call(this, path, props, options);
+            if (isResource(output)) {
+              res.json(output);
+            } else {
+              res.json(this._wrapInResource(output));
+            }
+          } catch (err) {
+            console.error(err instanceof Error ? err.stack : err);
+            res.sendStatus(500).send(err instanceof Error ? err.stack : err);
           }
-        } catch (err) {
-          console.error(err instanceof Error ? err.stack : err);
-          res.sendStatus(500).send(err instanceof Error ? err.stack : err);
-        }
+        });
       });
     });
   }
 
-  async handleService(path, props, options) {
+  _bindWebsocket() {
+    const clients = {};
+
+    this.websocketServer.on('connection', (ws) => {
+      ws.id = uuid.v4();
+      clients[ws.id] = ws;
+
+      ws.on('message', async (rawMessage) => {
+        //console.log('received from client: %s', rawMessage);
+        const message = JSON.parse(rawMessage);
+        if (typeof message !== 'object' || message === null) return;
+        if (message.type !== 'request' || !message.requestId || !message.path || !message.body) return;
+        const requestId = message.requestId;
+
+        try {
+          // message.path. e.g. "/service/math/foo" or just "/service"
+          const messagePathParts = message.path.split('/');
+          const type = messagePathParts[1];
+          const httpPath = messagePathParts.slice(2).join('/'); // e.g. "math/foo" or just ""
+          const path = httpToServicePath(httpPath); // e.g. "math.foo" or just ""
+
+          const props = message.body.props || {};
+          const options = message.body.options || {};
+
+          const output = await this.handlersByType[type].call(this, path, props, options);
+          let jsonOutput;
+          if (isResource(output)) {
+            jsonOutput = output;
+          } else {
+            jsonOutput = this._wrapInResource(output);
+          }
+
+          ws.send(JSON.stringify({
+            type: 'response',
+            status: 'success',
+            requestId,
+            body: jsonOutput,
+          }));
+        } catch (err) {
+          console.error(err instanceof Error ? err.stack : err);
+          ws.send(JSON.stringify({
+            type: 'response',
+            status: 'error',
+            requestId,
+            body: err instanceof Error ? err.stack : err,
+          }));
+        }
+      });
+
+      ws.send('something from server');
+    });
+  }
+
+  // wrap the output into a resource anyway, since we always want to send json
+  // (to handle things like numeric content and to simplify the client job)
+  _wrapInResource(output) {
+    return {
+      name: 'wrappedContent',
+      contentType: 'x-webmiddle-any',
+      content: output,
+    };
+  }
+
+  async _handleService(path, props, options) {
     if (!path) {
       return {
         name: 'services',
@@ -92,7 +160,7 @@ export default class Server {
     return evaluate(createContext(this.webmiddle, options), <Service {...props} />);
   }
 
-  async handleSetting(path) {
+  async _handleSetting(path) {
     if (!path) {
       return {
         name: 'settings',
--- packages/webmiddle-server/tests/index.js
+++ packages/webmiddle-server/tests/index.js
@@ -1,11 +1,15 @@
-import test from 'ava';
-import Server from '../src';
 import WebMiddle from 'webmiddle';
 import superagent from 'superagent';
+import WebSocket from 'ws';
+import uuid from 'uuid';
+import test from 'ava';
+import Server from '../src';
 
-function requestServer(method, url, data = {}) {
+const PORT = 3000;
+
+function requestExpress(method, path, data = {}) {
   return new Promise((resolve, reject) => {
-    superagent[method.toLowerCase()](url)
+    superagent[method.toLowerCase()]('http://localhost:' + PORT + path)
       .send(data)
       .end((err, res) => {
         if (err) reject(err);
@@ -14,7 +18,48 @@ function requestServer(method, url, data = {}) {
   });
 }
 
-const PORT = 3000;
+let wsPromise;
+function getWebsocketConnection() {
+  if (!wsPromise) {
+    wsPromise = new Promise((resolve, reject) => {
+      try {
+        const ws = new WebSocket(`ws://localhost:${PORT}/`);
+        ws.on('open', () => resolve(ws));
+        ws.on('error', reject);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+  return wsPromise;
+}
+
+function requestWebsocket(path, body = {}) {
+  return getWebsocketConnection().then(ws => {
+    return new Promise((resolve, reject) => {
+      try {
+        const requestId = uuid.v4();
+
+        ws.on('message', (rawMessage) => {
+          //console.log('received from server: %s', rawMessage);
+          const message = JSON.parse(rawMessage);
+          if (message.type !== 'response' || message.requestId !== requestId) return;
+
+          if (message.status === 'success') {
+            resolve(message.body);
+          } else if (message.status === 'error') {
+            reject(message.body);
+          }
+        });
+
+        ws.send(JSON.stringify({ type: 'request', requestId, path, body }));
+      } catch (err) {
+        console.error(err instanceof Error ? err.stack : err);
+        reject(err instanceof Error ? err.stack : err);
+      }
+    });
+  });
+}
 
 const webmiddle = new WebMiddle({
   services: {
@@ -22,7 +67,8 @@ const webmiddle = new WebMiddle({
       sum: ({ a, b }) => ({
         name: 'result',
         contentType: 'text/plain',
-        content: String(Number(a) + Number(b)), // without Number() a + b would be a string concatenation in GET requests!
+        // without Number() a + b would be a string concatenation in GET requests!
+        content: String(Number(a) + Number(b)),
       }),
 
       multiply: ({ a, b }) => ({
@@ -51,7 +97,7 @@ const server = new Server(webmiddle, { port: PORT });
 server.start();
 
 test('Execute service via GET', async t => {
-  const resource = await requestServer('GET', 'http://localhost:' + PORT + '/services/math/sum?a=5&b=10');
+  const resource = await requestExpress('GET', '/services/math/sum?a=5&b=10');
   t.deepEqual(resource, {
     name: 'result',
     contentType: 'text/plain',
@@ -60,7 +106,21 @@ test('Execute service via GET', async t => {
 });
 
 test('Execute service via POST', async t => {
-  const resource = await requestServer('POST', 'http://localhost:' + PORT + '/services/math/multiply', {
+  const resource = await requestExpress('POST', '/services/math/multiply', {
+    props: {
+      a: 20,
+      b: 5,
+    }
+  });
+  t.deepEqual(resource, {
+    name: 'result',
+    contentType: 'text/plain',
+    content: '100',
+  });
+});
+
+test('Execute service via WEBSOCKET', async t => {
+  const resource = await requestWebsocket('/services/math/multiply', {
     props: {
       a: 20,
       b: 5,
@@ -74,7 +134,20 @@ test('Execute service via POST', async t => {
 });
 
 test('Pass context options to service via POST', async t => {
-  const resource = await requestServer('POST', 'http://localhost:' + PORT + '/services/returnOption', {
+  const resource = await requestExpress('POST', '/services/returnOption', {
+    props: {
+      optionName: 'custom'
+    },
+    options: {
+      custom: 5
+    },
+  });
+  t.is(resource.contentType, 'x-webmiddle-any');
+  t.is(resource.content, 5);
+});
+
+test('Pass context options to service via WEBSOCKET', async t => {
+  const resource = await requestWebsocket('/services/returnOption', {
     props: {
       optionName: 'custom'
     },
@@ -87,19 +160,34 @@ test('Pass context options to service via POST', async t => {
 });
 
 test('Read setting via GET', async t => {
-  const resource = await requestServer('GET', 'http://localhost:' + PORT + '/settings/foo/other');
+  const resource = await requestExpress('GET', '/settings/foo/other');
   t.is(resource.contentType, 'x-webmiddle-any');
   t.is(resource.content, 100);
 });
 
 test('Read setting via POST', async t => {
-  const resource = await requestServer('GET', 'http://localhost:' + PORT + '/settings/foo/other');
+  const resource = await requestExpress('POST', '/settings/foo/other');
+  t.is(resource.contentType, 'x-webmiddle-any');
+  t.is(resource.content, 100);
+});
+
+test('Read setting via WEBSOCKET', async t => {
+  const resource = await requestWebsocket('/settings/foo/other');
   t.is(resource.contentType, 'x-webmiddle-any');
   t.is(resource.content, 100);
 });
 
 test('Get service paths', async t => {
-  const resource = await requestServer('GET', 'http://localhost:' + PORT + '/services/');
+  const resource = await requestExpress('GET', '/services/');
+  t.deepEqual(resource, {
+    name: 'services',
+    contentType: 'application/json',
+    content: ['math.sum', 'math.multiply', 'math.divide', 'returnOption']
+  });
+});
+
+test('Get service paths via WEBSOCKET', async t => {
+  const resource = await requestWebsocket('/services/');
   t.deepEqual(resource, {
     name: 'services',
     contentType: 'application/json',
@@ -108,7 +196,16 @@ test('Get service paths', async t => {
 });
 
 test('Get setting paths', async t => {
-  const resource = await requestServer('GET', 'http://localhost:' + PORT + '/settings/');
+  const resource = await requestExpress('GET', '/settings/');
+  t.deepEqual(resource, {
+    name: 'settings',
+    contentType: 'application/json',
+    content: ['foo.some', 'foo.other']
+  });
+});
+
+test('Get setting paths via WEBSOCKET', async t => {
+  const resource = await requestWebsocket('/settings/');
   t.deepEqual(resource, {
     name: 'settings',
     contentType: 'application/json',
