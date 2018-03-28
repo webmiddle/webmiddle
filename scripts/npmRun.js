11e663fc143d356f631ccadc2848dc99e1bb886c
Manuel Dell'Elce <mh025r0x@gmail.com>
2018-03-28 22:09:51 +0200

    Remove need for npx

---
+++ scripts/npmRun.js
@@ -0,0 +1,27 @@
+const { spawn } = require("child_process");
+const path = require("path");
+
+let command = process.argv[2];
+if (!command) {
+  throw new Error("No command specified");
+}
+// http://stackoverflow.com/a/17537559
+const cmdCommands = ["babel"];
+if (process.platform === "win32" && cmdCommands.indexOf(command) >= 0) {
+  command += ".cmd";
+}
+command = path.resolve(__dirname, "../node_modules/.bin/" + command);
+
+const commandArguments = process.argv.slice(3);
+
+console.log("Executing", command, commandArguments);
+const childProcess = spawn(command, commandArguments);
+childProcess.stdout.on("data", data => {
+  console.log(data.toString());
+});
+childProcess.stderr.on("data", data => {
+  console.log("\x1b[31m", data.toString());
+});
+childProcess.on("exit", code => {
+  //console.log(dir, 'child process exited with code ' + code.toString());
+});
